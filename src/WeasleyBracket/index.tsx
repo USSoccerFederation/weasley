c68279d0f313a2012cc1195466f1b7ecd536bd25
Wes Strong <wstrong@ussoccer.org>
2023-06-23 17:04:42 -0500

    WIP Populating teams by match

--- src/WeasleyBracket/index.tsx
+++ src/WeasleyBracket/index.tsx
@@ -1,6 +1,21 @@
 import styles from "@/styles/WeasleyBracket.module.scss";
+import db from '../../db.json';
+import Image from "next/image";
+import { Participant } from "../../types/Participant";
+import { Content } from "next/font/google";
 
 export default function WeasleyBracket() {
+    const data = db;
+    const participants = data?.participant;
+    const matches = data?.matches;
+
+    function findContestantById({ dataArray } : any, id : string) {
+        console.log("dataArray", dataArray);
+        return dataArray.find(({ x } : any) => {
+            return x.id === id ? x : null;
+        })
+    }
+
     return (
         <>
             <div className={styles.header}>
@@ -10,6 +25,29 @@ export default function WeasleyBracket() {
                 <div>Semifinal</div>
                 <div>Final</div>
             </div>
+            <div className={styles.round}>
+                {matches.map(match => (
+                    <div key={match.matchId} className={styles.matchup}>
+                        {match.contestants.map(contestant => (
+                            <li key={contestant.id}>
+                                {contestant.name}
+                            </li>
+                        ))}
+                    </div>
+                ))}
+            </div>
         </>
+    )
+}
+
+export function TeamLogo({ url }: Participant) {
+    return (
+        <Image
+            src={`/assets/${url}.png`}
+            width="0"
+            height="0"
+            alt="Team logo"
+            style={{ width: '2rem', height: 'auto', margin: '0 1rem' }}
+        />
     )
 }
