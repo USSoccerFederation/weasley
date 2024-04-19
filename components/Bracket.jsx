2363880acfd18b7e66319eaf497c0d9302af0cff
Wes Strong <wstrong@ussoccer.org>
2024-04-19 11:08:34 -0500

    Dynamically pulling in teams from USOC calendar endpoint

--- components/Bracket.jsx
+++ components/Bracket.jsx
@@ -1,10 +1,24 @@
 import React, { useEffect, useState, useRef } from 'react';
 import styles from "@/styles/Bracket.module.scss";
-import matches from "../data.json"
+import matches from "../data.json";
+import fetchMatches from "../utils/matches";
 
-export default function Bracket() {
+function Bracket() {
     const [selectedTeams, setSelectedTeams] = useState({});
-    const allMatches= matches;
+    const [allMatches, setAllMatches] = useState([]);
+
+    const fetchMatchData = async() => {
+        try {
+            const response = await fetchMatches();
+            setAllMatches(response.match);
+        } catch (error) {
+            console.error('Error fetching match data:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchMatchData();
+    }, []);
 
     const handleTeamSelect = (matchId, team) => {
         setSelectedTeams(prevState => ({
@@ -26,19 +40,19 @@ export default function Bracket() {
                     </tr>
                 </thead>
                 <tbody>
-                    {allMatches.map(match => (
-                        <tr key={match.Id} className={styles.column}>
+                    {allMatches?.map(match => (
+                        <tr key={match.matchInfo.id} className={styles.column}>
                             <td className={styles.match}>
-                                <span className={styles.id}>{match.Id}</span>
+                                <span className={styles.id}>{match.matchInfo.idd}</span>
                                 <div className={styles.teams}>
                                     <button 
                                         className={`${styles.team} ${selectedTeams[match.Id] === match.Team1 ? styles.selected : ''}`}
-                                        onClick={() => handleTeamSelect(match.Id, match.Team1)}
-                                    >{match.Team1}</button>
+                                        onClick={() => handleTeamSelect(match.matchInfo.id, match.matchInfo.contestant[0].name)}
+                                    >{match.matchInfo.contestant[0].name}</button>
                                     <button 
-                                        className={`${styles.team2} ${styles.team} ${selectedTeams[match.Id] === match.Team2 ? styles.selected : ''}`}
-                                        onClick={() => handleTeamSelect(match.Id, match.Team2)}
-                                    >{match.Team2}</button>
+                                        className={`${styles.team2} ${styles.team} ${selectedTeams[match.matchInfo.id] === match.matchInfo.contestant[1].name ? styles.selected : ''}`}
+                                        onClick={() => handleTeamSelect(match.matchInfo.id, match.matchInfo.contestant[1].name)}
+                                    >{match.matchInfo.contestant[1].name}</button>
                                 </div>
                             </td>
                         </tr>
@@ -48,3 +62,5 @@ export default function Bracket() {
         </div>
     )
 }
+
+export default Bracket;
---
+++ utils/matches.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+
+async function fetchMatches() {
+    const url = "https://ussf-opta-api-prod.azurewebsites.net/api/optaproxy?route=match%2F1svofrzot823s1rmatl9zhxqe8%3F_fmt%3Djson%26_rt%3Db%26comp%3D2bmwykmdlcc2u1c40ytoc39vy";
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+    }
+}
+
+export default fetchMatches;
