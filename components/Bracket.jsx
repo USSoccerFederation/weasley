152623fa08dd02dd0fe2c62dc4e8e22fd4580bde
Wes Strong <wstrong@ussoccer.org>
2024-07-11 09:12:16 -0500

    Working bracket with imported teams

--- components/Bracket.jsx
+++ components/Bracket.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "@/styles/Bracket.module.scss";
 import fetchMatches from "../utils/matches";
 
@@ -6,10 +6,10 @@ function Bracket() {
     const [selectedTeams, setSelectedTeams] = useState({});
     const [allMatches, setAllMatches] = useState([]);
 
-    const fetchMatchData = async() => {
+    const fetchMatchData = async () => {
         try {
             const response = await fetchMatches();
-            setAllMatches(response.match);
+            setAllMatches(response.match?.reverse());
         } catch (error) {
             console.error('Error fetching match data:', error);
         }
@@ -26,45 +26,89 @@ function Bracket() {
         }));
     };
 
+    const eigthFinals = allMatches?.filter(match => match?.matchInfo?.stage?.name === "8th Finals");
+    const quarterFinals = allMatches?.filter(match => match?.matchInfo?.stage?.name === "Quarter-finals");
+    const semiFinals = allMatches?.filter(match => match?.matchInfo?.stage?.name === "Semi-finals");
+    const final = allMatches?.filter(match => match?.matchInfo?.stage?.name === "Final");
+
+    const getTeamName = (team) => {
+        return team?.name || "TBD";
+    };
+
     return (
         <div className={styles.bracketContainer}>
-            <ul>
-                {Object.keys(selectedTeams).map((key) => (
-                    <li key={key}>
-                        {key}: {selectedTeams[key]}
-                    </li>
+            <div className={styles.stage}>
+                <h3>8th Finals</h3>
+                {eigthFinals?.map(({ matchInfo }, index) => (
+                    <div key={matchInfo.id} className={styles.match}>
+                        <span className={styles.id}>Game {index + 1}</span>
+                        <div className={styles.teams}>
+                            <button 
+                                className={`${styles.team} ${selectedTeams[matchInfo.id] === matchInfo.contestant[0].officialName ? styles.selected : ''}`}
+                                onClick={() => handleTeamSelect(matchInfo.id, matchInfo.contestant[0].name)}
+                            >{getTeamName(matchInfo.contestant?.[0])}</button>
+                            <button 
+                                className={`${styles.team} ${styles.team2} ${selectedTeams[matchInfo.id] === matchInfo.contestant[1].officialName ? styles.selected : ''}`}
+                                onClick={() => handleTeamSelect(matchInfo.id, matchInfo.contestant[1].name)}
+                            >{getTeamName(matchInfo.contestant?.[1])}</button>
+                        </div>
+                    </div>
                 ))}
-            </ul>
-            <table className={styles.bracket}>
-                <thead>
-                    <tr>
-                        <th className={styles.column}>Round of 32</th>
-                        <th className={styles.column}>Round of 16</th>
-                        <th className={styles.column}>Quarter Final</th>
-                        <th className={styles.column}>Semifinal</th>
-                        <th className={styles.column}>Final</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {allMatches?.map(({ matchInfo }, index) => (
-                        <tr key={matchInfo.id} className={styles.column}>
-                            <td className={styles.match}>
-                                <span className={styles.id}>Game {index + 1}</span>
-                                <div className={styles.teams}>
-                                    <button 
-                                        className={`${styles.team} ${selectedTeams[matchInfo.id] === matchInfo.contestant[0].officialName ? styles.selected : ''}`}
-                                        onClick={() => handleTeamSelect(matchInfo.id, matchInfo.contestant[0].name)}
-                                    >{matchInfo.contestant[0].name}</button>
-                                    <button 
-                                        className={`${styles.team2} ${styles.team} ${selectedTeams[matchInfo.id] === matchInfo.contestant[1].officialName ? styles.selected : ''}`}
-                                        onClick={() => handleTeamSelect(matchInfo.id, matchInfo.contestant[1].name)}
-                                    >{matchInfo.contestant[1].name}</button>
-                                </div>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            </div>
+            <div className={styles.stage}>
+                <h3>Quarter Finals</h3>
+                {quarterFinals?.map(({ matchInfo }, index) => (
+                    <div key={matchInfo.id} className={styles.match}>
+                        <span className={styles.id}>Game {index + 1}</span>
+                        <div className={styles.teams}>
+                            <button 
+                                className={`${styles.team} ${selectedTeams[matchInfo.id] === matchInfo.contestant[0].officialName ? styles.selected : ''}`}
+                                onClick={() => handleTeamSelect(matchInfo.id, matchInfo.contestant[0].name)}
+                            >{getTeamName(matchInfo.contestant?.[0])}</button>
+                            <button 
+                                className={`${styles.team} ${styles.team2} ${selectedTeams[matchInfo.id] === matchInfo.contestant[1].officialName ? styles.selected : ''}`}
+                                onClick={() => handleTeamSelect(matchInfo.id, matchInfo.contestant[1].name)}
+                            >{getTeamName(matchInfo.contestant?.[1])}</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.stage}>
+                <h3>Semi Finals</h3>
+                {semiFinals?.map(({ matchInfo }, index) => (
+                    <div key={matchInfo.id} className={styles.match}>
+                        <span className={styles.id}>Game {index + 1}</span>
+                        <div className={styles.teams}>
+                            <button 
+                                className={`${styles.team} ${selectedTeams[matchInfo.id] === matchInfo.contestant[0].officialName ? styles.selected : ''}`}
+                                onClick={() => handleTeamSelect(matchInfo.id, matchInfo.contestant[0].name)}
+                            >{getTeamName(matchInfo.contestant?.[0])}</button>
+                            <button 
+                                className={`${styles.team} ${styles.team2} ${selectedTeams[matchInfo.id] === matchInfo.contestant[1].officialName ? styles.selected : ''}`}
+                                onClick={() => handleTeamSelect(matchInfo.id, matchInfo.contestant[1].name)}
+                            >{getTeamName(matchInfo.contestant?.[1])}</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.stage}>
+                <h3>Final</h3>
+                {final?.map(({ matchInfo }, index) => (
+                    <div key={matchInfo.id} className={styles.match}>
+                        <span className={styles.id}>Game {index + 1}</span>
+                        <div className={styles.teams}>
+                            <button 
+                                className={`${styles.team} ${selectedTeams[matchInfo.id] === matchInfo.contestant[0].officialName ? styles.selected : ''}`}
+                                onClick={() => handleTeamSelect(matchInfo.id, matchInfo.contestant[0].name)}
+                            >{getTeamName(matchInfo.contestant?.[0])}</button>
+                            <button 
+                                className={`${styles.team} ${styles.team2} ${selectedTeams[matchInfo.id] === matchInfo.contestant[1].officialName ? styles.selected : ''}`}
+                                onClick={() => handleTeamSelect(matchInfo.id, matchInfo.contestant[1].name)}
+                            >{getTeamName(matchInfo.contestant?.[1])}</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
